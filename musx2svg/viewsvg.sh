4c6868ad766787b8fca639155ef15d86b231da21
thomas <thomas@13b5bd53-c693-4dc7-a195-f12dcceea0be>
2012-02-03 07:46:54 +0000

    Changed syntax of viewsvg.sh, added zoom setting
    git-svn-id: https://menotti.bib.hfm-detmold.de/svn/SVGRenderer@82 13b5bd53-c693-4dc7-a195-f12dcceea0be

--- musx2svg/viewsvg.sh
+++ musx2svg/viewsvg.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-# Syntax: viewsvg.sh [-bb] FILE
-# Option -bb adds bounding boxes and draws them in the SVG
+# Syntax: viewsvg.sh [options] FILE
+# Option are:
+# -b or --bounding-boxes
+#   adds bounding boxes and draws them in the SVG
+# -z=NUMBER or --zoom=NUMBER
+#   Set zoom factor (standard is 1)
 
 initialMode=""
+zoom=""
 
 for arg; do 
-  if [ "$arg" == "-bb" ] ; then
-    initialMode="-im:svg-with-bounding-boxes"
-  else
-    filename=$arg
-  fi
+  case $arg in
+    -b|--bounding-boxes)
+      initialMode="-im:svg-with-bounding-boxes"
+      ;;
+    -z=*|--zoom=*)
+      zoom="--x-zoom ${arg#*=} --y-zoom ${arg#*=}"
+      ;;
+    *)
+      filename=$arg
+      ;;
+  esac
 done
 
-saxonb-xslt -xsl:def2xsl.xsl -s:elementDefinitions/musx.def | saxonb-xslt -xsl:- $initialMode -s:$filename | rsvg-view -s
+saxonb-xslt -xsl:def2xsl.xsl -s:elementDefinitions/musx.def | saxonb-xslt -xsl:- $initialMode -s:$filename | rsvg-view --stdin $zoom
