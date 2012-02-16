55da23cfbf68a7f0a26d7451f40ab9ad9accaefd
thomas <thomas@13b5bd53-c693-4dc7-a195-f12dcceea0be>
2012-02-16 09:04:13 +0000

    shell script with complete conversion chain
    git-svn-id: https://menotti.bib.hfm-detmold.de/svn/SVGRenderer@114 13b5bd53-c693-4dc7-a195-f12dcceea0be

---
+++ mei2svg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+xsltproc preprocessor/add-ids.xsl $1 | \
+xsltproc preprocessor/canonicalize.xsl - | \
+saxonb-xslt -xsl:preprocessor/addDurations.xsl -s:- | \
+saxonb-xslt -xsl:preprocessor/addSynchronicity.xsl -s:- |\
+saxonb-xslt -xsl:mei2musx/mei2musx.xsl -s:- |\
+saxonb-xslt -xsl:musx2svg/musx2svg.xsl -s:-
