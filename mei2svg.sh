fd46db201a1337c1851c7cff9ce3c430eb270aa1
thomas <thomas@13b5bd53-c693-4dc7-a195-f12dcceea0be>
2012-02-25 09:40:12 +0000

    added stylesheets reduceToMdiv.xsl and spacing.xsl
    git-svn-id: https://menotti.bib.hfm-detmold.de/svn/SVGRenderer@131 13b5bd53-c693-4dc7-a195-f12dcceea0be

--- mei2svg.sh
+++ mei2svg.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+saxonb-xslt -xsl:preprocessor/reduceToMdiv.xsl -s:$1 |\
 xsltproc preprocessor/add-ids.xsl $1 | \
 xsltproc preprocessor/canonicalize.xsl - | \
 saxonb-xslt -xsl:preprocessor/addDurations.xsl -s:- | \
 saxonb-xslt -xsl:preprocessor/addSynchronicity.xsl -s:- |\
 saxonb-xslt -xsl:mei2musx/mei2musx.xsl -s:- |\
 saxonb-xslt -xsl:formatter/accidentalFormatter.xsl -s:- |\
+saxonb-xslt -xsl:formatter/spacing.xsl -s:- |\
 saxonb-xslt -xsl:musx2svg/musx2svg.xsl -s:-
